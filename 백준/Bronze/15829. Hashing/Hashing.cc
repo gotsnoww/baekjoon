c0e3f95a8b229110a1f8545a0e5cd2a2867d8825
gotsnoww <127213266+gotsnoww@users.noreply.github.com>
2023-06-27 12:37:02 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#define M 1234567891
 
 int main() {
 	int L;
@@ -8,17 +9,13 @@ int main() {
 	scanf("%d", &L);
 	scanf("%s", apb);
 
-	long long int H = 0;
-	int M = 1234567891;
-	int r = 31;
+	long long H = 0;
+	long long r = 1;
 
 	int str_length = strlen(apb);
 	for (int i = 0; i < str_length; i++) {
-		int p = 1;
-		for (int j = 0; j < i; j++)
-			p *= r;
-
-		H += ((apb[i] - 96) * p);
+		H += ((apb[i] - 96) * r) % M;
+		r = (r * 31) % M;
 	}
 
 	printf("%lld", H % M);
